77ae3dedd7abf2502d0ec28c54816a7c7aed8cc9
paul <paulbrowne2@gmail.com>
2022-03-01 13:35:09 +0000

    Connected mongodb with error handlingand referenced mongo client and db in MongoInstance struct

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/mongocrypt/options"
+)
 
 type MongoInstance struct {
-	Client
-	Db
+	Client *mongo.Client
+	Db     *mongo.Database
 }
 
 var mg MongoInstance
@@ -20,13 +29,34 @@ type Employee struct {
 }
 
 func Connect() error {
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
 
+	err = client.Connect(ctx)
+	db := client.Databse(dbName)
+
+	if err != nil {
+		return err
+	}
+
+	mg = MongoInstance{
+		Client: client,
+		Db:     db,
+	}
+	return nil
 }
 
 func main() {
+
+	if err := Connect(); err != nil {
+		log.Fatal(err)
+	}
 	app := fiber.New()
 
-	app.Get("/employee")
+	app.Get("/employee", func(c *fiber.Ctx) error {
+
+	})
 	app.Post("/employee")
 	app.Put("/employee/:id")
 	app.Delete("/employee/:id")
